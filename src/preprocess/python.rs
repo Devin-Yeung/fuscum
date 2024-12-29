8d7864d5e9953ae6ee8d6b99224f08c674dcd250
Devin Yeung <devin.cg.yeung.dev@outlook.com>
2024-12-29 14:30:08 +0800

    feat: make the `tree` impl generic

--- src/preprocess/python.rs
+++ src/preprocess/python.rs
@@ -33,14 +33,16 @@ impl Default for PythonPreprocessor {
     }
 }
 
-pub struct PyTree {
-    ag: AstGrep<StrDoc<Python>>,
+pub struct Tree<L: Language> {
+    lang: L,
+    ag: AstGrep<StrDoc<L>>,
 }
 
-impl PyTree {
-    pub fn new<S: AsRef<str>>(src: S) -> Self {
+impl<L: Language + Copy> Tree<L> {
+    pub fn new<S: AsRef<str>>(src: S, lang: L) -> Self {
         Self {
-            ag: Python.ast_grep(src.as_ref()),
+            ag: lang.ast_grep(src.as_ref()),
+            lang,
         }
     }
 
@@ -66,26 +68,26 @@ impl PyTree {
             0 => self,
             _ => {
                 let new_content = self.apply_edit_helper(edits);
-                self.ag = Python.ast_grep(&new_content);
+                self.ag = self.lang.ast_grep(&new_content);
                 self
             }
         }
     }
 
-    pub fn remove_comments(&mut self) -> &mut Self {
-        let pat = KindMatcher::new("comment", Python);
+    pub fn remove_comments<S: AsRef<str>>(&mut self, kind: S) -> &mut Self {
+        let pat = KindMatcher::new("comment", self.lang);
         let edits = self.ag.root().replace_all(&pat, "");
         self.apply_edits(edits)
     }
 
-    pub fn subst_ident(&mut self, to: &str) -> &mut Self {
-        let pat = KindMatcher::new("identifier", Python);
+    pub fn subst_ident<S: AsRef<str>>(&mut self, kind: S, to: &str) -> &mut Self {
+        let pat = KindMatcher::new(kind.as_ref(), self.lang);
         let edits = self.ag.root().replace_all(&pat, to);
-        self.apply_edits(edits) // TODO: bottleneck, it's too slow
+        self.apply_edits(edits)
     }
 
-    pub fn subst_string(&mut self, to: &str) -> &mut Self {
-        let pat = KindMatcher::new("string", Python);
+    pub fn subst_string<S: AsRef<str>>(&mut self, kind: S, to: &str) -> &mut Self {
+        let pat = KindMatcher::new(kind.as_ref(), self.lang);
         let edits = self.ag.root().replace_all(&pat, to);
         self.apply_edits(edits)
     }
@@ -97,19 +99,19 @@ impl PyTree {
 
 impl Preprocessor for PythonPreprocessor {
     fn preprocess<S: AsRef<str>>(&self, src: &S) -> Cow<str> {
-        let mut tree = PyTree::new(src);
+        let mut tree = Tree::new(src, Python);
 
         if self.remove_comments {
-            tree.remove_comments();
+            tree.remove_comments("comment");
         }
 
         self.subst_var
             .as_ref()
-            .map(|v| tree.subst_ident(v.as_str()));
+            .map(|v| tree.subst_ident("identifier", v));
 
         self.subst_string
             .as_ref()
-            .map(|v| tree.subst_string(v.as_str()));
+            .map(|v| tree.subst_string("string", v));
         // remove all the whitespace in the source code
         let src = tree
             .source()
@@ -126,14 +128,14 @@ mod tests {
 
     #[test]
     fn var_subst() {
-        let mut tree = PyTree::new("def f(a, b, c):\n\ta = 1");
-        tree.subst_ident("v");
+        let mut tree = Tree::new("def f(a, b, c):\n\ta = 1", Python);
+        tree.subst_ident("identifier", "v");
         assert_eq!(tree.source(), "def v(v, v, v):\n\tv = 1");
     }
 
     #[test]
     fn preprocess() {
-        let src = "def f(a, b, c):\n\ta = \"hello\"";
+        let src = "def f(a, b, c):\n\ta = \"hello\" # comment";
         let pp = PythonPreprocessor::builder()
             .subst_var("v")
             .subst_string("\"s\"")
