66d96fd92845bef1febb912ef44592be0d8bf3cf
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2025-02-06 12:19:24 +0000

    feat: setup devenv

---
+++ devenv.nix
@@ -0,0 +1,43 @@
+{ pkgs, lib, config, inputs, ... }:
+
+{
+  # https://devenv.sh/packages/
+  packages = [ pkgs.git ];
+
+  # https://devenv.sh/languages/
+  languages.rust.enable = true;
+
+  # https://devenv.sh/processes/
+  # processes.cargo-watch.exec = "cargo-watch";
+
+  # https://devenv.sh/services/
+  # services.postgres.enable = true;
+
+  # https://devenv.sh/scripts/
+  enterShell = ''
+    git --version
+    rustc --version
+  '';
+
+  # https://devenv.sh/tasks/
+  # tasks = {
+  #   "myproj:setup".exec = "mytool build";
+  #   "devenv:enterShell".after = [ "myproj:setup" ];
+  # };
+
+  # https://devenv.sh/tests/
+  enterTest = ''
+    echo "Running tests"
+    git --version | grep --color=auto "${pkgs.git.version}"
+    rustc --version | grep --color=auto "${pkgs.rustc.version}"
+  '';
+
+  # https://devenv.sh/pre-commit-hooks/
+  # pre-commit.hooks.shellcheck.enable = true;
+  git-hooks.hooks = {
+    clippy.enable = true;
+    rustfmt.enable = true;
+  };
+
+  # See full reference at https://devenv.sh/reference/options/
+}
