ce065e4b1749be345986f3a5af8acf4176e4a9d6
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-09 17:37:07 +0800

    chore: bumping devenv

--- devenv.nix
+++ devenv.nix
@@ -13,23 +13,11 @@
   # https://devenv.sh/languages/
   languages.rust.enable = true;
 
-  # https://devenv.sh/processes/
-  # processes.cargo-watch.exec = "cargo-watch";
-
-  # https://devenv.sh/services/
-  # services.postgres.enable = true;
-
   # https://devenv.sh/scripts/
   enterShell = ''
     git --version
     rustc --version
   '';
-
-  # https://devenv.sh/tasks/
-  # tasks = {
-  #   "myproj:setup".exec = "mytool build";
-  #   "devenv:enterShell".after = [ "myproj:setup" ];
-  # };
 
   # https://devenv.sh/tests/
   enterTest = ''
@@ -40,9 +28,13 @@
 
   # https://devenv.sh/pre-commit-hooks/
   # pre-commit.hooks.shellcheck.enable = true;
-  git-hooks.hooks = {
-    clippy.enable = true;
-    rustfmt.enable = true;
+  git-hooks = {
+    hooks = {
+      clippy.enable = true;
+      rustfmt.enable = true;
+      nixfmt.enable = true;
+    };
+    package = pkgs.prek;
   };
 
   # See full reference at https://devenv.sh/reference/options/
