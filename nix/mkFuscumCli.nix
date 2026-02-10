e9dec6ec5a48bc5ff02516b16310ebc1c19288dd
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-10 14:43:46 +0800

    build(nix): install fuscum-cli shell completions

--- nix/mkFuscumCli.nix
+++ nix/mkFuscumCli.nix
@@ -5,10 +5,21 @@
 let
   sharedArgs = import ./mkSharedArgs.nix { inherit pkgs craneLib; };
 
+  inherit (pkgs) lib stdenv buildPackages;
   inherit (sharedArgs) individualCrateArgs;
+
+  fuscum-cli = "${stdenv.hostPlatform.emulator buildPackages} $out/bin/fuscum-cli";
 in
 individualCrateArgs
 // {
   pname = "fuscum-cli";
   cargoExtraArgs = "-p fuscum-cli";
+  nativeBuildInputs = with pkgs; [ installShellFiles ];
+
+  postInstall = lib.optionalString (stdenv.hostPlatform.emulatorAvailable buildPackages) ''
+    installShellCompletion --cmd fuscum-cli \
+      --bash <(${fuscum-cli} completions bash) \
+      --fish <(${fuscum-cli} completions fish) \
+      --zsh <(${fuscum-cli} completions zsh)
+  '';
 }
