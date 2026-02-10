a72066751e23d54b7f9e69349caa75a4b90a746a
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-10 18:44:37 +0800

    refactor: simplify nix build

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "Building static binaries with musl";
+  description = "Fuscum: A Rust implementation of MOSS for code plagiarism detection";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
@@ -9,22 +9,112 @@
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
-    inputs@{ flake-parts, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
-      ];
-      imports = [
-        ./flakes/build.nix
-        ./flakes/build-static.nix
-        ./flakes/checks.nix
-      ];
-    };
+    {
+      nixpkgs,
+      crane,
+      flake-utils,
+      rust-overlay,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [ (import rust-overlay) ];
+        };
+        craneLib = (crane.mkLib pkgs).overrideToolchain (
+          p: p.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml
+        );
+        inherit (pkgs) lib;
+
+        unfilteredRoot = ./.;
+        src = lib.fileset.toSource {
+          root = unfilteredRoot;
+          fileset = lib.fileset.unions [
+            (craneLib.fileset.commonCargoSources unfilteredRoot)
+            (lib.fileset.fileFilter (file: file.hasExt "snap") unfilteredRoot)
+            (lib.fileset.fileFilter (file: file.hasExt "html") unfilteredRoot)
+            (lib.fileset.maybeMissing ./fixtures)
+          ];
+        };
+
+        commonArgs = {
+          inherit src;
+          pname = "fuscum-cli";
+          cargoExtraArgs = "-p fuscum-cli";
+          strictDeps = true;
+          nativeBuildInputs = with pkgs; [ installShellFiles ];
+          buildInputs = lib.optionals pkgs.stdenv.isDarwin [
+            pkgs.libiconv
+          ];
+        };
+
+        cargoArtifacts = craneLib.buildDepsOnly commonArgs;
+
+        fuscum-cli = craneLib.buildPackage (
+          commonArgs
+          // {
+            inherit cargoArtifacts;
+            postInstall =
+              let
+                fuscum-cli-bin = "${pkgs.stdenv.hostPlatform.emulator pkgs.buildPackages} $out/bin/fuscum-cli";
+              in
+              lib.optionalString (pkgs.stdenv.hostPlatform.emulatorAvailable pkgs.buildPackages) ''
+                installShellCompletion --cmd fuscum-cli \
+                  --bash <(${fuscum-cli-bin} completions bash) \
+                  --fish <(${fuscum-cli-bin} completions fish) \
+                  --zsh <(${fuscum-cli-bin} completions zsh)
+              '';
+          }
+        );
+      in
+      {
+        checks = {
+          inherit fuscum-cli;
+
+          fuscum-clippy = craneLib.cargoClippy (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              cargoClippyExtraArgs = "--all-targets -- --deny warnings";
+            }
+          );
+
+          fuscum-doc = craneLib.cargoDoc (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              env.RUSTDOCFLAGS = "--deny warnings";
+            }
+          );
+
+          fuscum-fmt = craneLib.cargoFmt {
+            inherit src;
+          };
+
+          fuscum-toml-fmt = craneLib.taploFmt {
+            src = lib.sources.sourceFilesBySuffices src [ ".toml" ];
+          };
+
+          fuscum-nextest = craneLib.cargoNextest (
+            commonArgs
+            // {
+              inherit cargoArtifacts;
+              partitions = 1;
+              partitionType = "count";
+              cargoNextestPartitionsExtraArgs = "--no-tests=pass";
+            }
+          );
+        };
+
+        packages = {
+          inherit fuscum-cli;
+          default = fuscum-cli;
+        };
+      }
+    );
 }
--- nix/default.nix
+++
@@ -1,7 +0,0 @@
-{
-  mkCrossCraneLib = import ./mkCrossCraneLib.nix;
-  mkCrossPkgs = import ./mkCrossPkgs.nix;
-  mkFuscumCli = import ./mkFuscumCli.nix;
-  mkSharedArgs = import ./mkSharedArgs.nix;
-  crossBuild = import ./crossBuild.nix;
-}
