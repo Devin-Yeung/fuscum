2a5517bccb713d45c6bf57ca1d547e674d9989ef
Devin-Yeung <53309384+Devin-Yeung@users.noreply.github.com>
2026-02-10 14:16:20 +0800

    chore: cleanup devenv

--- devenv.nix
+++ devenv.nix
@@ -34,12 +34,9 @@
   # https://devenv.sh/tests/
   enterTest = ''
     echo "Running tests"
-    git --version | grep --color=auto "${pkgs.git.version}"
-    rustc --version | grep --color=auto "${pkgs.rustc.version}"
   '';
 
   # https://devenv.sh/pre-commit-hooks/
-  # pre-commit.hooks.shellcheck.enable = true;
   git-hooks = {
     hooks = {
       clippy = {
